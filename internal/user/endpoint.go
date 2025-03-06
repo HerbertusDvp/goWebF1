f096f533efd613b8e49c3113bb8a5d8c97aaa981
herbert <Herbertus@gmail.com>
2025-03-05 21:42:39 -0600

    Feat: Controlador de peticiones

--- internal/user/endpoint.go
+++ internal/user/endpoint.go
@@ -1,6 +1,64 @@
 package user
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
 type (
+	Controller func(w http.ResponseWriter, r *http.Request)
+
 	EndPoints struct {
+		Create Controller
+		Get    Controller
+		GetAll Controller
+		Update Controller
+		Delete Controller
 	}
 )
+
+func MakeEndpoints() EndPoints {
+
+	return EndPoints{
+		Create: makeCreateEndpoint(),
+		Get:    makeGetEndpoint(),
+		GetAll: makeGetAllEndpoint(),
+		Update: makeUpdateEndpoint(),
+		Delete: makeDeleteEndpoint(),
+	}
+
+}
+
+func makeCreateEndpoint() Controller {
+	return func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+	}
+}
+
+func makeGetEndpoint() Controller {
+	return func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+	}
+}
+
+func makeGetAllEndpoint() Controller {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Make GetAll endpoint")
+		json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+	}
+}
+
+func makeUpdateEndpoint() Controller {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Make update endpoint")
+		json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+	}
+}
+
+func makeDeleteEndpoint() Controller {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Make delete endpoint")
+		json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+	}
+}
